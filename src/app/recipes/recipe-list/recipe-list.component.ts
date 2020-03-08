45e4528617056bba54b5fa714cec5c6c14b7ab81
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-08 16:44:53 +0330

    working on recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model' ;
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe('test','testing','http://coastguard.dodlive.mil/files/2014/06/IMG_7999.jpg')
+  ];
 
   constructor() { }
 
