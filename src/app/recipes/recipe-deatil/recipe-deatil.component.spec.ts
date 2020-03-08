e642b7640b2d8c6d02cad9107f4908722c121589
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-08 15:09:30 +0330

    adding header

---
+++ src/app/recipes/recipe-deatil/recipe-deatil.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeDeatilComponent } from './recipe-deatil.component';
+
+describe('RecipeDeatilComponent', () => {
+  let component: RecipeDeatilComponent;
+  let fixture: ComponentFixture<RecipeDeatilComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipeDeatilComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipeDeatilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
